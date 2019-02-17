363dc828b7349bf16e492597c59d0a5944e2063f
jack hammer <jackhammer@localhost.localdomain>
2019-02-17 21:44:33 +0700

    Add main file

---
+++ src/Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "mySimpleComputer.h"
+
+int main(void) 
+{
+    sc_memoryInit();
+    sc_memorySet (76, 8);
+    sc_memorySet (4, 543634);
+    sc_memorySet (765, 543634);
+    sc_memorySet (26, 8657);
+    sc_memoryShow();
+    sc_memorySave ("Comp.dat");
+    sc_memoryInit();
+    sc_memoryLoad ("Comp.dat");
+    sc_memoryShow();
+}
