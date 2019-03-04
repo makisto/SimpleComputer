72122d1c103dded337d621149ae4bd8a4af6cf26
makisto <TJAYYM@yandex.ru>
2019-03-04 23:48:22 +0700

    Add files for tests

---
+++ test/test.c
@@ -0,0 +1,104 @@
+#define CTEST_MAIN
+
+#include "mySimpleComputer.h"
+#include <ctest.h>
+
+int operand = 0x50;
+int command;
+
+CTEST(adress, Correct)
+{
+    int result = sc_memorySet (34, 55);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(adress, Incorrect)
+{
+    int result = sc_memorySet (884, 55);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(value, Correct)
+{
+    int result = sc_memoryGet (34, &result);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(value, Incorrect)
+{
+    int result = sc_memoryGet (884, &result);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}  
+
+CTEST(flag, Right)
+{
+    int result = sc_regSet (OVERFLOW, 1);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+} 
+
+CTEST(flag, Wrong)
+{
+    int result = sc_regSet (OVERFLOW, 75);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+} 
+
+CTEST(flag, Correct)
+{
+    int result = sc_regGet (OVERFLOW, &result);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+} 
+
+CTEST(flag, Incorrect)
+{
+    int result = sc_regGet (11, &result);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(command, Correct)
+{
+    int result = sc_commandEncode(0x10, operand, &result);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(command, Incorrect)
+{
+    int result = sc_commandEncode(0x99, operand, &result);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(command, Right)
+{
+    int result = sc_commandDecode(2256, &command, &operand);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(command, Wrong)
+{
+    int result = sc_commandDecode(0xFFFF, &command, &operand);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(comm, Wrong)
+{
+    int result = sc_commandDecode(0x66, &command, &operand);
+    int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+int main(int argc, const char** argv) 
+{
+    int test_res = ctest_main(argc, argv);
+    return test_res;
+}
