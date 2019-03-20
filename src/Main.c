b6a6d646a27c7480b82e7a456f8a5cc8241deef7
makisto <TJAYYM@yandex.ru>
2019-03-20 08:39:10 +0700

    Add printBigChars on console

--- src/Main.c
+++ src/Main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include "mySimpleComputer.h"
 #include "myTerm.h"
 #include "myBigChars.h"
@@ -8,6 +10,7 @@
 int main(void) 
 {    
     system("tput reset");
+
     memoryShow();
     printBoxes();
     CPUshow();
@@ -15,4 +18,8 @@ int main(void)
     keysShow();
     int mas[] = {1, 1};
     bc_printbigchar(mas, 15, 2, GREEN, GREEN);
+    bc_printbigchar(mas, 15, 17, GREEN, GREEN);
+    bc_printbigchar(mas, 15, 26, GREEN, GREEN);
+    bc_printbigchar(mas, 15, 35, GREEN, GREEN);
+    bc_printbigchar(mas, 15, 44, GREEN, GREEN);
 }
